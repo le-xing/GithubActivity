e3151a28184fbe91da063369c2d61027403d006b
Bryan Ngo <bngo@ucdavis.edu>
2017-03-14 15:44:37 -0700

    Added homework 4 implementation

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -22,6 +22,11 @@ public class GithubQuerier {
     public static String eventsAsHTML(String user) throws IOException, ParseException {
         List<JSONObject> response = getEvents(user);
         StringBuilder sb = new StringBuilder();
+
+        JSONObject json, singleCommit;
+        JSONArray commits;
+        String SHA, message;
+
         sb.append("<div>");
         for (int i = 0; i < response.size(); i++) {
             JSONObject event = response.get(i);
@@ -47,7 +52,30 @@ public class GithubQuerier {
             sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
             sb.append(event.toString());
             sb.append("</pre> </div>");
+            sb.append("<div>");
+
+            json = event.getJSONObject("payload");
+            commits = json.getJSONArray("commits");
+
+            for(int j = 0; j < commits.length(); j++) {
+
+                singleCommit = commits.getJSONObject(j);
+                SHA = singleCommit.getString("sha");
+                message = singleCommit.getString("message");
+
+                sb.append("<strong>" + "SHA: " + "</strong>" + SHA + "<br>");
+                sb.append("<strong>"+ "Message: " + "</strong>" + message);
+                sb.append("<br>");
+                sb.append("<hr>");
+
+            }
+
+
+            sb.append("</div>");
+
         }
+
+
         sb.append("</div>");
         return sb.toString();
     }
@@ -58,10 +86,22 @@ public class GithubQuerier {
         System.out.println(url);
         JSONObject json = Util.queryAPI(new URL(url));
         System.out.println(json);
+
+        JSONObject object;
+        String eventType;
         JSONArray events = json.getJSONArray("root");
-        for (int i = 0; i < events.length() && i < 10; i++) {
-            eventList.add(events.getJSONObject(i));
+        for (int i = 0; i < events.length(); i++) {
+
+            object = events.getJSONObject(i);
+            eventType = object.getString("type");
+
+            if(eventType.equals("PushEvent"))
+                eventList.add(events.getJSONObject(i));
+
+            if(eventList.size() == 10) break;
+
         }
         return eventList;
     }
+
 }
