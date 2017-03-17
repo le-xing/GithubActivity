6bcf03feeea3662e623764f57760aa6cb71343da
Lena Tan <lentan127@gmail.com>
2017-03-17 02:26:03 -0700

    Moved doswap.sh to HW4 directory

--- HW4/doswap.sh
+++ HW4/doswap.sh
@@ -15,13 +15,13 @@ then
     if grep --quiet web1 /tmp/ctr$$
     then
         docker run -d --network ecs189_default --name web2 $1
-        docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh
+        sleep 5 && docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh
         docker rm -f `docker ps -a | grep web1 | sed -e 's: .*$::'`
 
     elif grep --quiet web2 /tmp/ctr$$
     then
         docker run -d --network ecs189_default --name web1 $1
-        docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh
+        sleep 5 && docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh
         docker rm -f `docker ps -a | grep web2 | sed -e 's: .*$::'`
     fi
 else
